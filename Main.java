0ddb274d52b501f129b43a2a59ba1df3036791a3
assassinmert <65746645+assassinmert@users.noreply.github.com>
2020-06-18 08:27:32 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         JFrame w = new JFrame();
          w.setSize(850,600);
     //if you want the title to change, alter the text in the quotes below
-         w.setTitle("Title Change");
+         w.setTitle("Faster Than a Susanoo");
 //         w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //         w.setExtendedState(JFrame.MAXIMIZED_BOTH);
          GUI m = new GUI();
